76623c6620c6f19fe478aae5a847e7037b7d1706
Sulzbach99 <lucas@sulzbach.org>
2017-11-01 20:34:19 -0200

    Common

---
+++ common.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+/* Implementação de um malloc com verificação */
+
+void *Malloc(int size)
+{
+    void *ptr;
+    ptr = malloc(size);
+    if (!ptr)
+    {
+        fprintf(stderr, "Malloc error\n");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
+
+/**********************************************/
---
+++ common.h
@@ -0,0 +1,9 @@
+#ifndef __COMMON__
+#define __COMMON__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *Malloc(int size);
+
+#endif
