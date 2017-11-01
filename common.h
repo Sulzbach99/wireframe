3872153c9d3150de1be81cd23a8b3d98195ec3d9
Sulzbach99 <lucas@sulzbach.org>
2017-11-01 21:02:13 -0200

    Inclusão dos datatypes em common.h

--- common.h
+++ common.h
@@ -1,6 +1,29 @@
 #ifndef __COMMON__
 #define __COMMON__
 
+/* Coordenada bidimensional */
+
+typedef struct {
+
+    float x;
+    float y;
+
+} twoD_t;
+
+/****************************/
+
+/* Coordenada tridimensional */
+
+typedef struct {
+
+    float x;
+    float y;
+    float z;
+
+} threeD_t;
+
+/*****************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 
