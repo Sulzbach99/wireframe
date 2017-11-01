ed18f13734a102f18d8a37a9e1a3f1d643aa2b09
Sulzbach99 <lucas@sulzbach.org>
2017-11-01 21:02:19 -0200

    API de lists

---
+++ lists.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/* Cria uma lista genérica com ponteiros First e Last iguais a NULL */
+
+void createList(list_t *List)
+{
+    List = Malloc(sizeof(list_t));
+    FIRSTCELL(List) = NULL;
+    LASTCELL(List) = FIRSTCELL(List); // João 20:16
+}
+
+/********************************************************************/
+
+/* Cria uma célula no final da lista dada                             /
+** Se é a primeira célula da lista, o ponteiro First aponta para ela */
+
+void createCell(list_t *List)
+{
+    NEXTCELL(LASTCELL(List)) = Malloc(sizeof(cell_t));
+    LASTCELL(List) = NEXTCELL(LASTCELL(List));
+    NEXTCELL(LASTCELL(List)) = NULL;
+
+    if (!(FIRSTCELL(List)))
+        FIRSTCELL(List) = LASTCELL(List);
+}
+
+/*********************************************************************/
+
+/* Remove a primeira célula da lista dada */
+
+void removeCell(list_t *List)
+{
+    if (!(FIRSTCELL(List)))
+    {
+        fprintf(stderr, "Error: Tried to remove cell from empty list\n");
+        exit(EXIT_FAILURE);
+    }
+
+    cell_t *ptr;
+    ptr = FIRSTCELL(List);
+    FIRSTCELL(List) = NEXTCELL(FIRSTCELL(List));
+    free(ptr);
+}
+
+/******************************************/
---
+++ lists.h
@@ -0,0 +1,35 @@
+#ifndef __LISTS__
+#define __LISTS__
+
+#include "common.h"
+
+/* Definição de uma lista genérica */
+
+typedef struct cell {
+
+    void *Item;
+    struct cell *Next;
+
+} cell_t;
+
+typedef struct {
+
+    cell_t *First;
+    cell_t *Last;
+
+} list_t;
+
+/***********************************/
+
+/* Constantes para facilitar a sintaxe */
+
+#define FIRSTCELL(list) list->First
+#define LASTCELL(list) list->Last
+#define NEXTCELL(cell) cell->Next
+
+/***************************************/
+
+void createList(list_t *List);
+void createCell(list_t *List);
+
+#endif
